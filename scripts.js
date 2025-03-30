a46f4e55a8dc127e61a2f2e81e24fa1bcaef86cc
KentseMatshira <kmatshira@yahoo.com>
2025-03-30 19:34:24 +0200

    fix DOMload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
+// @ts-check
+
 import { books, authors, genres, BOOKS_PER_PAGE } from './data.js';
 
-let page = 1;
-let matches = books;
+class BookPreview {
+    constructor(book) {
+        this.book = book;
+        this.element = this.createElement();
+    }
 
-class Book {
+    createElement() {
+        const { author, id, image, title } = this.book;
+        const element = document.createElement('button');
+        element.classList.add('preview');
+        element.setAttribute('data-preview', id);
+        element.innerHTML = `
+            <img class="preview__image" src="${image}" />
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `;
+        return element;
+    }
+}
+
      /**
       * Create a Book instance.
       * @param {string} id
